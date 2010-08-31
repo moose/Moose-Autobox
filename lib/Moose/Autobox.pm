a34c4a981feecfcbb44e91b1a31b294d67aa9b11
Karen Etheridge <ether@cpan.org>
2010-08-31 10:25:52 -0700

    clarify the nature of the performance penalty

--- lib/Moose/Autobox.pm
+++ lib/Moose/Autobox.pm
@@ -112,9 +112,12 @@ the 'hooks' for others to add implementation too.
 Several people are using this module in serious applications and 
 it seems to be quite stable. The underlying technologies of L<autobox>
 and L<Moose::Role> are also considered stable. There is some performance
-hit, but as I am fond of saying, nothing in life is free. If you have 
-any questions regarding this module, either email me, or stop by #moose
-on irc.perl.org and ask around.
+hit, but as I am fond of saying, nothing in life is free.  Note that this hit
+only applies to the I<use> of methods on native Perl values, not the mere act
+of loading this module in your namespace.
+
+If you have any questions regarding this module, either email me, or stop by
+#moose on irc.perl.org and ask around.
 
 =head2 Adding additional methods
 
